7b9e1682f69b362f563f36f9a985f9ad27a3910f
ProgrammerFront077 <alirezakh0090@gmail.com>
2023-09-24 17:19:59 +0330

    (Add):<- Client , Server Source->

---
+++ client/app/page.tsx
@@ -0,0 +1,17 @@
+import CourseGroup from './Components/Course/CourseGroup';
+import { asyncGetCourse } from './Helpers/Course';
+
+const Home = async () => {
+  try {
+    const courses = await asyncGetCourse();
+    return (
+      <div className="grid grid-cols-1 gap-3">
+        <CourseGroup courses={courses} />
+      </div>
+    );
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
+
+export default Home;
